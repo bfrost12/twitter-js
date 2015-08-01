fab6eea3726d4989b79855ab712cc6b9d30870db
Bryce <bfrost08@gmail.com>
2015-08-01 12:42:54 -0400

    Created a basic template

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+//we need express! and morgan middleware!
 var express = require( 'express' );
-var app = express();
 var morgan = require('morgan');
-
+var app = express();
 app.use(morgan('dev'));
 
+//we need swig! to make things look great!
+var swig = require('swig');
+swig.setDefaults({ cache: false });
+
+
+//set up app to use Swig
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.listen(3000, function(){
